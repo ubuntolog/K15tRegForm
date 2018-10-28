e38e91b746c8e6bf4a5efd7a69c61e81a4cb86a9
Alexandr Chernov <kstchernov@gmail.com>
2018-10-28 19:03:35 +0100

    Added email validation

--- src/main/java/com/k15t/pat/registration/RegistrationController.java
+++ src/main/java/com/k15t/pat/registration/RegistrationController.java
@@ -5,6 +5,7 @@ import com.k15t.pat.json.User;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -31,7 +32,7 @@ public class RegistrationController {
             @RequestParam(name="name") @NotBlank(message="Name cannot be empty") String name,
             @RequestParam(name="password") @Size(min=6, message="Password should have at least 6 characters") String password,
             @RequestParam(name="address") @NotBlank(message="Address cannot be empty") String address,
-            @RequestParam(name="email") @NotBlank(message="Email cannot be empty") String email,
+            @RequestParam(name="email") @Email(message="Not a well-formed email address") @NotBlank(message="Email cannot be empty") String email,
             @RequestParam(name="phone", required=false, defaultValue="") String phone
         ) {
 
