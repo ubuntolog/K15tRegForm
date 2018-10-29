6811be4a06abed195de8a9211611d63117d9f0f9
Alexandr Chernov <kstchernov@gmail.com>
2018-10-29 21:13:51 +0100

    Added a test for the API info endpoint

---
+++ src/test/java/com/k15t/pat/registration/RegistrationControllerTests.java
@@ -0,0 +1,40 @@
+package com.k15t.pat.registration;
+
+import com.k15t.pat.ApplicationBootstrap;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {ApplicationBootstrap.class})
+@WebAppConfiguration
+
+public class RegistrationControllerTests {
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getApiInfoTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/info"))
+                .andExpect(status().isOk());
+    }
+
+}
