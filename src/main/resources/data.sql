b90af790b3dbb589bbec134b464a8528f203707f
Alexandr Chernov <kstchernov@gmail.com>
2018-11-01 19:45:30 +0100

    Displaying the list of visitors on the frontend

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into visitor values(1,'John Smith','1234567','Stuttgarter strasse 1','john@smith.com','123');
+insert into visitor (id, name, password, address, email, phone) values(1,'John Smith','1234567','Stuttgarter strasse 1','john@smith.com','123');
 
-insert into visitor values(2,'Jack Smith','1234567','Stuttgarter strasse 2','jack@smith.com','123');
+insert into visitor (id, name, password, address, email, phone) values(2,'Jack Smith','1234567','Stuttgarter strasse 2','jack@smith.com','123');
 
-insert into visitor values(3,'Jane Smith','1234567','Stuttgarter strasse 3','jane@smith.com','123');
+insert into visitor (id, name, password, address, email, phone) values(3,'Jane Smith','1234567','Stuttgarter strasse 3','jane@smith.com','123');
--- src/main/resources/public/webui/src/components/Registration.jsx
+++ src/main/resources/public/webui/src/components/Registration.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FormGroup, ControlLabel, FormControl, Button, Glyphicon, ButtonToolbar, Modal, HelpBlock} from 'react-bootstrap';
+import {Grid, Col, Row, FormGroup, ControlLabel, FormControl, Button, Glyphicon, ButtonToolbar, Modal, HelpBlock, Tabs, Tab} from 'react-bootstrap';
 
 const formFields = ["name", "password", "address", "email", "phone"];
+const VisitorsTableRow = ({name, email}) => {
+    return (
+        <Row style={{marginTop:10}}>
+            <Col xs={2} sm={2} md={2} lg={2} >
+                {name}
+            </Col>
+            <Col xs={2} sm={2} md={2} lg={2} >
+                {email}
+            </Col>
+        </Row>
+    );
+}
 class Registration extends React.Component {
     constructor(props) {
         super(props);
 
+        this.handleSelect = this.handleSelect.bind(this);
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
 
         this.state = {
+            key: 1,
             show: false
         };
     }
+
+    handleSelect(key) {
+        this.setState({ key });
+      }
 
     handleClose() {
         this.setState({ show: false });
@@ -61,99 +79,123 @@ class Registration extends React.Component {
         }
         return(
                 <div>
-                    <form onSubmit={this.handleSubmit.bind(this)}>
-                    <FormGroup
-                        controlId="nameGroup"
-                        validationState={validationStatus[formFields[0]]["state"]}
+                    <Tabs
+                        activeKey={this.state.key}
+                        onSelect={this.handleSelect}
+                        id="controlled-tab-example"
                     >
-                        <ControlLabel>Name</ControlLabel>
-                        <FormControl
-                            type="text"
-                            placeholder="Enter your name"
-                            name={formFields[0]}                                        
-                        />
-                        <FormControl.Feedback />
-                        <HelpBlock>{validationStatus[formFields[0]]["message"]}</HelpBlock>
-                    </FormGroup>
+                        <Tab eventKey={1} title="Enter your data">
+                            <form onSubmit={this.handleSubmit.bind(this)}>
+                            <FormGroup
+                                controlId="nameGroup"
+                                validationState={validationStatus[formFields[0]]["state"]}
+                            >
+                                <ControlLabel>Name</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter your name"
+                                    name={formFields[0]}                                        
+                                />
+                                <FormControl.Feedback />
+                                <HelpBlock>{validationStatus[formFields[0]]["message"]}</HelpBlock>
+                            </FormGroup>
 
-                    <FormGroup
-                        controlId="passwordGroup"
-                        validationState={validationStatus[formFields[1]]["state"]}
-                    >
-                        <ControlLabel>Password</ControlLabel>
-                        <FormControl
-                            type="password" 
-                            placeholder="Enter your password"
-                            name={formFields[1]}                                        
-                        />
-                        <FormControl.Feedback />
-                        <HelpBlock>{validationStatus[formFields[1]]["message"]}</HelpBlock>
-                    </FormGroup>
+                            <FormGroup
+                                controlId="passwordGroup"
+                                validationState={validationStatus[formFields[1]]["state"]}
+                            >
+                                <ControlLabel>Password</ControlLabel>
+                                <FormControl
+                                    type="password" 
+                                    placeholder="Enter your password"
+                                    name={formFields[1]}                                        
+                                />
+                                <FormControl.Feedback />
+                                <HelpBlock>{validationStatus[formFields[1]]["message"]}</HelpBlock>
+                            </FormGroup>
 
-                    <FormGroup
-                        controlId="addressGroup"
-                        validationState={validationStatus[formFields[2]]["state"]}
-                    >
-                        <ControlLabel>Address</ControlLabel>
-                        <FormControl
-                            type="text"
-                            placeholder="Enter your address"
-                            name={formFields[2]}                                       
-                        />
-                        <FormControl.Feedback />
-                        <HelpBlock>{validationStatus[formFields[2]]["message"]}</HelpBlock>
-                    </FormGroup>
+                            <FormGroup
+                                controlId="addressGroup"
+                                validationState={validationStatus[formFields[2]]["state"]}
+                            >
+                                <ControlLabel>Address</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter your address"
+                                    name={formFields[2]}                                       
+                                />
+                                <FormControl.Feedback />
+                                <HelpBlock>{validationStatus[formFields[2]]["message"]}</HelpBlock>
+                            </FormGroup>
 
-                    <FormGroup
-                        controlId="emailGroup"
-                        validationState={validationStatus[formFields[3]]["state"]}
-                    >
-                        <ControlLabel>Email</ControlLabel>
-                        <FormControl
-                            type="text"
-                            placeholder="Enter your email address"
-                            name={formFields[3]}                                        
-                        />
-                        <FormControl.Feedback />
-                        <HelpBlock>{validationStatus[formFields[3]]["message"]}</HelpBlock>
-                    </FormGroup>
-                    
-                    <FormGroup
-                        controlId="phoneGroup"
-                        validationState={validationStatus[formFields[4]]["state"]}
-                    >
-                        <ControlLabel>Phone number</ControlLabel>
-                        <FormControl
-                            type="text"
-                            placeholder="Enter your phone number"
-                            name={formFields[4]}                                       
-                        />
-                        <FormControl.Feedback />
-                        <HelpBlock>{validationStatus[formFields[4]]["message"]}</HelpBlock>
-                    </FormGroup>
-                    <ButtonToolbar>
-                        <Button type="submit">Register</Button>
-                        <Button title="Get help" onClick={this.handleShow}>
-                            <Glyphicon glyph="info-sign" />
-                        </Button>
-                    </ButtonToolbar>                    
-                </form> 
+                            <FormGroup
+                                controlId="emailGroup"
+                                validationState={validationStatus[formFields[3]]["state"]}
+                            >
+                                <ControlLabel>Email</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter your email address"
+                                    name={formFields[3]}                                        
+                                />
+                                <FormControl.Feedback />
+                                <HelpBlock>{validationStatus[formFields[3]]["message"]}</HelpBlock>
+                            </FormGroup>
+                            
+                            <FormGroup
+                                controlId="phoneGroup"
+                                validationState={validationStatus[formFields[4]]["state"]}
+                            >
+                                <ControlLabel>Phone number</ControlLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter your phone number"
+                                    name={formFields[4]}                                       
+                                />
+                                <FormControl.Feedback />
+                                <HelpBlock>{validationStatus[formFields[4]]["message"]}</HelpBlock>
+                            </FormGroup>
+                            <ButtonToolbar>
+                                <Button type="submit">Register</Button>
+                                <Button title="Get help" onClick={this.handleShow}>
+                                    <Glyphicon glyph="info-sign" />
+                                </Button>
+                            </ButtonToolbar>                    
+                        </form> 
 
 
-                <Modal show={this.state.show} onHide={this.handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>About this application</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <h4>User registration form {this.props.apiinfo.version}</h4>
-                        <p>
-                            This application was developed by {this.props.apiinfo.author}
-                        </p>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button onClick={this.handleClose}>Close</Button>
-                    </Modal.Footer>
-                </Modal>
+                        <Modal show={this.state.show} onHide={this.handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>About this application</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <h4>User registration form {this.props.apiinfo.version}</h4>
+                                <p>
+                                    This application was developed by {this.props.apiinfo.author}
+                                </p>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button onClick={this.handleClose}>Close</Button>
+                            </Modal.Footer>
+                        </Modal>
+                    </Tab>
+                    <Tab eventKey={2} title="See who else is comming">
+                        <Grid>
+                            <Row>
+                                <Col xs={2} sm={2} md={2} lg={2} >Name</Col>
+                                <Col xs={2} sm={2} md={2} lg={2} >Email</Col>
+                            </Row>
+                            {  this.props.visitors.map(visitor =>
+                                <VisitorsTableRow
+                                    key={visitor.id}
+                                    name={visitor.name}
+                                    email={visitor.email}
+                                />
+                            )
+                            }
+                        </Grid>
+                    </Tab>
+                </Tabs>
             </div>
         )
     }
